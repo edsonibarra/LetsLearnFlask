ddcac16ffc09341183d6e9fae2ca0f6004aa5b45
edsonibarra <edson.ibarra@globant.com>
2023-11-21 17:01:00 -0600

    get_incomes endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, jsonify
 
 
 """
@@ -7,9 +7,18 @@ To run the server:
     - flask run
 """
 
+incomes = [
+    {"description": "salary", "amount": 5000}
+]
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/incomes")
+def get_incomes():
+    return jsonify(incomes)
